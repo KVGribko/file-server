ad5f43e1f54833e04125f91873e4d07b108ff0b5
Konstantin Gribko <czarkost@yandex.ru>
2023-08-29 15:19:22 +0300

    user

--- app/schemas/auth/user.py
+++ app/schemas/auth/user.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
 class User(BaseModel):
     username: str
+    dt_created: datetime
+    dt_updated: datetime
 
     class Config:
         orm_mode = True
